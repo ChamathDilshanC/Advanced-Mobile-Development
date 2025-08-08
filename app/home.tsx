02574f551c234084f41147555901569133405259
ChamathDilshanC <dilshancolonne123@gmail.com>
2025-08-08 09:06:28 +0530

    Improve Home page button styles and layout
    Enhanced the visual appearance of navigation buttons on the Home page by adding custom styles, padding, and colors. Updated layout for better spacing and alignment, and improved text styling for clarity and emphasis.

--- app/home.tsx
+++ app/home.tsx
@@ -1,47 +1,59 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
-
 import { Link } from "expo-router";
 import Footer from "./footer";
 
-const Home
- = () => {
+const Home = () => {
   return (
     <>
       <View
         style={{
           flex: 1,
           width: "100%",
-          // backgroundColor: "#e74c3c",
           justifyContent: "center",
           alignItems: "center",
+          padding: 20,
         }}
       >
-        <Link className="text-2xl" href={"/project"}>
-          Project
-        </Link>
-        <Link className="text-2xl" href={"/user"}>
-          User
-        </Link>
-        <Link className="text-2xl" href={"/login"}>
-          Login
-        </Link>
         <Link
-          className="text-2xl"
+          className="items-center w-48 px-6 py-4 mb-4 bg-blue-600 rounded-lg shadow-lg"
+          href={"/project"}
+        >
+          <Text className="text-lg font-semibold text-center text-white">
+            Project
+          </Text>
+        </Link>
+
+        <Link
+          className="items-center w-48 px-6 py-4 mb-4 text-center bg-gray-100 border-2 border-gray-300 rounded-lg"
+          href={"/user"}
+        >
+          <Text className="text-lg font-semibold text-gray-700">User</Text>
+        </Link>
+
+        <Link
+          className="items-center w-48 px-6 py-4 mb-4 text-center bg-green-600 rounded-full shadow-md"
+          href={"/login"}
+        >
+          <Text className="text-lg font-semibold text-white">Login</Text>
+        </Link>
+
+        <Link
+          className="items-center w-48 px-6 py-4 mb-4 text-center bg-purple-600 border border-purple-400 shadow-lg rounded-xl"
           href={{
             pathname: "/(item)/[id]",
             params: {
               id: "1234",
-              name : "Chamath",
-              age : "21",
-              address : "Colombo",
+              name: "Chamath",
+              age: "21",
+              address: "Colombo",
             },
           }}
         >
-          Selected Item
+          <Text className="text-lg font-bold text-white">Selected Item</Text>
         </Link>
 
-        <Text className="text-4xl text-red-700">This is index</Text>
+        <Text className="mt-4 text-4xl text-red-700">This is index</Text>
       </View>
       <Footer />
     </>

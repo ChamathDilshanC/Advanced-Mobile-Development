e397f6e92c434bd4455560e2b23fa7bcd71f3ccb
ChamathDilshanC <dilshancolonne123@gmail.com>
2025-08-21 12:17:51 +0530

    Add edit and delete functionality to tasks
    Implemented task editing and deletion in the dashboard UI and service layer. Added getTaskById, updateTask, and deleteTask methods to taskservice, and updated the UI to support editing and deleting tasks with improved styling.

--- services/taskservice.ts
+++ services/taskservice.ts
@@ -1,6 +1,6 @@
 import { db } from "@/firebase";
 import { Task } from "@/types/task";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
 
 export const getTasks = async (): Promise<Task[]> => {
   try {
@@ -49,6 +49,45 @@ export const addTask = async (task: {
 };
 
 
-export const updateTask =  () => {
-  
+export const updateTask =  (id: string,task : Task) => {
+  const { id: _id, ...taskData } = task; // remove id
+
+  const updateRef = doc(db, "tasks", id);
+  // title , description , createdAt ,id
+  return updateDoc(updateRef, {
+    title: task.title,
+    description: task.description,
+    createdAt: task.createdAt,
+  });
+
+  //   return updateDoc(updateRef, taskData); // <--- Uncomment this line to use taskData
+}
+
+
+
+export const deleteTask = (id: string) => {
+  const deleteRef = doc(db, "tasks", id);
+  return deleteDoc(deleteRef);
+};
+
+
+export const getTaskById = async (id: string) => {
+  try {
+    const docRef = doc(db, "tasks", id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+      return {
+        id: docSnap.id,
+        title: data.title,
+        description: data.description ?? "",
+        createdAt: data.createdAt ?? null,
+      };
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching task from Firestore:", error);
+    throw new Error("Failed to fetch task");
+  }
 }
